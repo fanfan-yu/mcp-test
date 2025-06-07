51caf787920f7a80ae4a2f306120e27b50ce0c33
fanfan-yu <2228998656@qq.com>
2025-06-07 21:42:15 +0800

    feat: implement the redis mcp server

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Python MCP Server Starter
 
-![Python](https://img.shields.io/badge/python-3.7%2B-blue)
+![Python](https://img.shields.io/badge/python-3.13%2B-blue)
 ![License](https://img.shields.io/badge/license-MIT-green)
 
 A beginner-friendly Python implementation of an MCP (Minecraft Protocol) server, designed to help newcomers understand Minecraft server development fundamentals.
@@ -9,7 +9,7 @@ A beginner-friendly Python implementation of an MCP (Minecraft Protocol) server,
 
 ## 📋 Prerequisites
 
-- Python 3.11+
+- Python 3.13+
 - pip package manager
 
 ## ⚡ Quick Start
@@ -27,19 +27,49 @@ pip install -r requirements.txt
 
 ### 3. Run the server
 ```bash
-python main.py
+python server.py
+```
+## 📚 Start with mcp inspector
+
+### 1. Clone the repository
+```bash
+git clone git@github.com:fanfan-yu/mcp-test.git
+cd mcp-test
 ```
 
-## 🛠️ TODO Project Structure
+### 2. Launch MCP Inspector
+Ensure Node.js (with npm and npx) is installed.
+
+```bash
+npx @modelcontextprotocol/inspector@0.9
+```
+#### Next Steps
+- Keep the terminal running
+- Open http://localhost:6274 in your browser
+
+### 3. Test in mcp inspector
+
+In the browser interface:
+
+| Configuration     | Value           | Description                     |
+|-------------------|-----------------|---------------------------------|
+| Transport Type    | `STDIO`         | Select standard I/O protocol    |
+| Command           | `uv`            | Ensure [uv](https://github.com/astral-sh/uv) is installed |
+| Arguments         | `run server.py` | Execution parameters            |
+
+
+### 4. Verify
+- Click 'List Tools'
+- Click 'redis_execution'
+- Set 'input' and click 'Run Tool'
+
+## 🛠️ Project Structure
 
 ```
-.
-├── main.py            # Server entry point
-├── protocol/          # MCP protocol implementation
-│   ├── packets.py     # Packet definitions
-│   └── handler.py     # Packet handling logic
-├── utils/             # Helper utilities
-└── requirements.txt   # Dependencies
+├── README.md - Project documentation
+├── requirements.txt - Dependencies
+├── server.py - Server main file
+└── config.yaml - Server demo configuration
 ```
 
 ## 📌 TODO List
---
+++ server.py
@@ -0,0 +1,98 @@
+import asyncio
+import logging
+import yaml
+import os
+
+import redis
+
+from mcp import stdio_server
+from mcp.server import Server
+from mcp.types import Tool, TextContent
+
+def load_config():
+    # read config.yaml
+    config_path = os.path.join(os.path.dirname(__file__), "config.yaml")
+    with open(config_path, "r", encoding="utf-8") as f:
+        config = yaml.safe_load(f)
+
+    # extract redis config
+    redis_config = config.get("redis", {})
+    return redis_config
+
+# build redis client
+def build_redis_client():
+    conf = load_config()
+    redisClient = redis.Redis(
+        host=conf.get("host", "localhost"),
+        port=conf.get("port", 6379),
+        password=conf.get("password", ''),
+        decode_responses=conf.get("decode_responses", True)
+    )
+    if not redisClient.ping():
+        raise ConnectionError("Failed to connect to Redis")
+
+    return redisClient
+
+logger = logging.getLogger(__name__)
+
+app = Server("mcp_demo")
+
+# singleton pattern
+client = build_redis_client()
+
+@app.list_tools()
+async def list_tools() -> list[Tool]:
+    logger.info("start to list tools.")
+    return [
+        Tool(
+            name="redis_execution",
+            description="Execute the redis command.",
+            inputSchema={
+                "type": "object",
+                "properties": {
+                    "input": {
+                        "type": "string",
+                        "description": "The input is the redis command to be executed."}
+                },
+                "required": ["input"]
+            }
+        )
+    ]
+
+@app.call_tool()
+async def call_tool(name: str, arguments: dict) -> list[TextContent]:
+    logger.info("start to call tool.")
+
+    if name != "redis_execution":
+        raise ValueError("Tool not found")
+
+    redis_execution_sql = arguments.get("input")
+    if not redis_execution_sql:
+        raise ValueError("Execution sql is required")
+
+    parts = redis_execution_sql.split()
+    command = parts[0].upper()
+    args = parts[1:]
+
+    try:
+        client.execute_command(command, *args)
+        return [TextContent(type="text", text=f"Execute sql successfully.")]
+    except redis.exceptions.ResponseError as e:
+        return [TextContent(type="text", text=f"Error executing sql: {str(e)}")]
+
+
+async def main():
+    logger.info("Start mcp server.")
+    async with stdio_server() as (read_stream, write_stream):
+        try:
+            await app.run(
+                read_stream,
+                write_stream,
+                app.create_initialization_options()
+            )
+        except Exception as e:
+            logger.error(f"Server error: {str(e)}", exc_info=True)
+            raise
+
+if __name__ == "__main__":
+    asyncio.run(main())
